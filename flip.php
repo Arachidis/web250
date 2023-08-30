aedeeeca5b98cd6a234db0c94ade435b275ff895
Arachidis <protohedron@gmail.com>
2023-08-30 06:20:59 -0400

    Completed flip.php

---
+++ flip.php
@@ -0,0 +1,32 @@
+<style>
+	.coin {
+		background: #999999;
+		color: #333333;
+		border-radius: 50%;
+		padding: 50px;
+		text-align: center;
+		font-size: 2rem;
+		font-weight: bold;
+		width: 50px;
+	}
+</style>
+
+<?php
+
+function flip() {
+	// Challenge: define this function 
+  if (rand(0, 1) == 0) {
+    //Heads 
+    return "\u{1f600}";
+  }
+  else {
+    //Tails
+    return "\u{1f985}";
+  } 
+}
+
+?>
+
+<div class="coin">
+	<?php echo flip(); ?>
+</div>
