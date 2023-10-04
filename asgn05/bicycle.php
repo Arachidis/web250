82242c7019c491c7e523dcc21cb6b1b266518c31
Arachidis <protohedron@gmail.com>
2023-10-04 08:42:30 -0400

    Starts work on asgn05

---
+++ asgn05/bicycle.php
@@ -0,0 +1,42 @@
+<?php 
+
+Class Bicycle {
+  public $brand;
+  public $model;
+  public $year;
+  public $category;
+  public $color;
+  public $description;
+  public $gender;
+  public $price;
+  public $weight_kg;
+  public $condition_id;
+  public $CATEGORIES;
+  public $GENDERS;
+  public $CONDITION_OPTIONS;
+
+  public function __construct($args) {
+
+  }
+
+  public function weight_kg($weight_kg) {
+
+  }
+  
+  public function set_weight_kg($weight_kg) {
+
+  }
+  
+  public function weight_lbs() {
+
+  }
+  
+  public function set_weight_lbs($weight_kg) {
+
+  }
+  
+  public function condition() {
+
+  }
+  
+}
