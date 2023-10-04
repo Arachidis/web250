627a1bc50bf76c3113efc18f55cba81ac71f5957
Arachidis <protohedron@gmail.com>
2023-10-04 10:48:00 -0400

    Completes the basic Bycycle class

---
+++ asgn05/private/classes/bicycle.class.php
@@ -0,0 +1,60 @@
+<?php 
+
+Class Bicycle {
+  public $brand;
+  public $model;
+  public $year;
+  public $category;
+  public $color;
+  public $description;
+  public $gender;
+  public $price;
+  protected $weight_kg;
+  protected $condition_id;
+
+  public const CATEGORIES = array('road','mountain','hybrid','cruiser','city','BMX');
+
+  public const GENDERS = array('men','women','unisex');
+
+  protected const CONDITION_OPTIONS = array(1 => 'Beat up', 2 => 'Decent', 3 => 'Good', 4 => 'Great', 5 => 'Like New');
+
+  public function __construct($args=[]) {
+    $this->brand = $args['brand'] ?? NULL;
+    $this->model = $args['model'] ?? NULL;
+    $this->year = $args['year'] ?? NULL;
+    $this->category = $args['category'] ?? NULL;
+    $this->color = $args['color'] ?? NULL;
+    $this->description = $args['description'] ?? NULL;
+    $this->price = $args['price'] ?? NULL;
+    $this->weight_kg = $args['weight_kg'] ?? NULL;
+    $this->condition_id = $args['condition_id'] ?? NULL;
+  }
+
+  public function weight_kg() {
+    return $this->weight_kg;
+  }
+  
+  public function set_weight_kg($weight_kg) {
+    $this->weight_kg = $weight_kg;
+  }
+  
+  public function weight_lbs() {
+    return $this->weight_kg * 2.204;
+  }
+  
+  public function set_weight_lbs($weight_lbs) {
+    $this->weight_kg = $weight_lbs / 2.204;
+  }
+  
+  public function condition($condition_id) {
+    if ($this->condition_id > 0) {
+      return Bicycle::CONDITION_OPTIONS[$this->condition_id];
+    }
+    else {
+      return 'Unknown';
+    }
+  }
+  
+}
+
+//EOF
