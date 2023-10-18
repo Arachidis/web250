1a7d31ff6ad27b2d24dae7f9b6718991aecdd834
Arachidis <protohedron@gmail.com>
2023-10-18 11:55:24 -0400

    Minor changes for uploading to a server

--- asgn07-oop-db-intro/private/db_credentials.php
+++ asgn07-oop-db-intro/private/db_credentials.php
@@ -2,7 +2,7 @@
 
 //Keep credentials in separate file
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "pickle");
+define("DB_SERVER", "");
+define("DB_USER", "");
+define("DB_PASS", "");
 define("DB_NAME", "chain_gang");
