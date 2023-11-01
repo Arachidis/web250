b3df4efacd7fbd3bae9c4a92662564c5248a546f
Arachidis <protohedron@gmail.com>
2023-11-01 12:25:01 -0400

    Minor changes for uploading to a server

--- asgn09-CRUD/private/db_credentials.php
+++ asgn09-CRUD/private/db_credentials.php
@@ -5,9 +5,9 @@
 // 2. Unique credentials on development and production servers
 // 3. Unique credentials if working with multiple developers
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "pickle");
+define("DB_SERVER", "");
+define("DB_USER", "");
+define("DB_PASS", "");
 define("DB_NAME", "chain_gang");
 
 ?>
